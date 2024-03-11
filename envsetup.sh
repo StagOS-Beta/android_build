8b8876ac0e2d96c40a841ac5170b75bbfae2d4b1
LuK1337 <priv.luk@gmail.com>
2024-03-11 11:28:46 +0100

    envsetup: Pass valid TARGET_RELEASE to check_product
    Change-Id: I709248c0d362cffdac57d9893b7dfb4d2329204c

--- envsetup.sh
+++ envsetup.sh
@@ -212,7 +212,7 @@ function check_product()
     export STAG_BUILD
 
         TARGET_PRODUCT=$1 \
-        TARGET_RELEASE= \
+        TARGET_RELEASE=$2 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
         TARGET_BUILD_APPS= \
@@ -825,8 +825,7 @@ function lunch()
         return 1
     fi
 
-
-    if ! check_product $product
+    if ! check_product $product $release
     then
         # if we can't find the product, try to grab it from our github
         T=$(gettop)
@@ -843,7 +842,7 @@ function lunch()
         cd $T > /dev/null
         vendor/stag/build/tools/roomservice.py $product
         cd - > /dev/null
-        check_product $product
+        check_product $product $release
     else
         T=$(gettop)
         cd $T > /dev/null
